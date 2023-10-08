3e46dd532d49f74f21eadfb5056c6e208eb521d0
Bryan Ndjeutcha <ndjeutcha@gmail.com>
2023-10-08 12:49:20 -0400

    Completer les fonctions

--- Board.java
+++ Board.java
@@ -10,7 +10,7 @@ class Board
 
     // Ne pas changer la signature de cette méthode
     public Board() {
-
+        board = new Mark[3][3];
     }
 
     // Place la pièce 'mark' sur le plateau, à la
@@ -18,7 +18,10 @@ class Board
     //
     // Ne pas changer la signature de cette méthode
     public void play(Move m, Mark mark){
+        int row = m.getRow();
+        int col = m.getCol();
 
+        this.board[row][col] = mark;
 
     }
 
@@ -28,6 +31,88 @@ class Board
     //           0   pour un match nul
     // Ne pas changer la signature de cette méthode
     public int evaluate(Mark mark){
+        Mark opPlayer;
+        switch (mark) {
+            case X:
+                opPlayer = Mark.O;
+                break;
+            case O:
+                opPlayer = Mark.X;
+                break;
+            default:
+                opPlayer = Mark.EMPTY;
+                break;
+        }
 
+        int playerScore = simpleEval(mark);
+
+        if (playerScore == 0) {
+            return -simpleEval(opPlayer);
+        } else {
+            return playerScore;
+        }
+    }
+
+    public int simpleEval(Mark mark){
+        for (int i = 0; i < this.board.length; i++) {
+            Mark ca = get(i, 0);
+            Mark cb = get(i, 1);
+            Mark cc = get(i, 2);
+
+            if (ca.equals(mark) && cb.equals(mark) && cc.equals(mark)) {
+                return 100;
+            }
+
+            Mark ra = get(i, 0);
+            Mark rb = get(i, 1);
+            Mark rc = get(i, 2);
+
+
+            if (ra.equals(mark) && rb.equals(mark) && rc.equals(mark)) {
+                return 100;
+            }
+        }
+
+        Mark la = get(0, 0);
+        Mark ce = get(1, 1);
+        Mark lc = get(2, 2);
+
+        Mark ra = get(0, 2);
+        Mark rc = get(2, 0);
+
+        if (la.equals(mark) && ce.equals(mark) && lc.equals(mark)) {
+            return 100;
+        }
+
+        if (ra.equals(mark) && ce.equals(mark) && rc.equals(mark)) {
+            return 100;
+        }
+        
+        return 0;
+    }
+
+    public Mark get(int row, int col) {
+        return this.board[col][row];
+    }
+
+    public ArrayList<Move> listMoves(Mark mark) {
+        ArrayList<Move> listMoves = new ArrayList<>();
+
+        for (int row = 0; row < this.board.length; row ++) {
+            for (int col = 0; col < this.board[row].length; col ++) {
+                if (!get(row, col).equals(mark)) {
+                    listMoves.add(new Move(row, col));
+                }
+            }
+        }
+        
+        return listMoves;
+    }
+
+    public Board clone() {
+        Board board = new Board();
+        board.board = this.board.clone();
+
+        return board;
     }
 }
--- CPUPlayer.java
+++ CPUPlayer.java
@@ -7,6 +7,8 @@ import java.util.ArrayList;
 class CPUPlayer
 {
 
+    private Mark player;
+
     // Contient le nombre de noeuds visités (le nombre
     // d'appel à la fonction MinMax ou Alpha Beta)
     // Normalement, la variable devrait être incrémentée
@@ -16,7 +18,7 @@ class CPUPlayer
     // Le constructeur reçoit en paramètre le
     // joueur MAX (X ou O)
     public CPUPlayer(Mark cpu){
-
+        this.player = cpu;
     }
 
     // Ne pas changer cette méthode
@@ -29,16 +31,136 @@ class CPUPlayer
     // ont le même score.
     public ArrayList<Move> getNextMoveMinMax(Board board)
     {
-        numExploredNodes = 0;
+        numExploredNodes = 1;
 
+        ArrayList<Move> moves = board.listMoves(player);
+
+        ArrayList<Float> scoreList = new ArrayList<>(moves.stream().map((move) -> minMax(board.clone(), move, numExploredNodes, player)).toList());
+
+        for (Float score : scoreList) {
+            if (scoreList.indexOf(score) != scoreList.lastIndexOf(score)) {
+                return moves;
+            } 
+        }
+
+        return new ArrayList<>();
     }
 
     // Retourne la liste des coups possibles.  Cette liste contient
     // plusieurs coups possibles si et seuleument si plusieurs coups
     // ont le même score.
     public ArrayList<Move> getNextMoveAB(Board board){
-        numExploredNodes = 0;
+        numExploredNodes = 1;
 
+        ArrayList<Move> moves = board.listMoves(player);
+
+        ArrayList<Float> scoreList = new ArrayList<>(moves.stream().map((move) -> alphaBeta(board.clone(), move, numExploredNodes, Float.NEGATIVE_INFINITY, Float.POSITIVE_INFINITY, player)).toList());
+
+        for (Float score : scoreList) {
+            if (scoreList.indexOf(score) != scoreList.lastIndexOf(score)) {
+                return moves;
+            } 
+        }
+
+        return new ArrayList<>();
+    }
+
+    public float alphaBeta(Board board, Move move, int depth, float alpha, float beta, Mark maxPlayer) {
+        board.play(move, maxPlayer);
+
+        if (depth == 0) {
+            return board.evaluate(maxPlayer);
+        }
+
+        float maxEval;
+        float minEval;
+
+        Mark opPlayer;
+
+        switch (maxPlayer) {
+            case X:
+                opPlayer = Mark.O;
+                break;
+            case O:
+                opPlayer = Mark.X;
+                break;
+            default:
+                opPlayer = Mark.EMPTY;
+                break;
+        }
+
+        if (player.equals(maxPlayer)) {
+            maxEval = Float.NEGATIVE_INFINITY;
+
+            for (Move childMove: board.listMoves(maxPlayer)) {
+                float eval = alphaBeta(board.clone(), childMove, depth - 1, alpha, beta, opPlayer);
+                maxEval = Float.max(maxEval, eval);
+                alpha = Float.max(alpha, eval);
+                if (beta <= alpha) {
+                    break;
+                } 
+            }
+
+            return maxEval;
+        } else {
+            minEval = Float.POSITIVE_INFINITY;
+
+            for (Move childMove: board.listMoves(maxPlayer)) {
+                float eval = alphaBeta(board.clone(), childMove, depth - 1, alpha, beta, opPlayer);
+                minEval = Float.min(minEval, eval);
+                beta = Float.min(beta, eval);
+                if (beta <= alpha) {
+                    break;
+                }
+            }
+
+            return minEval;
+        }
+    }
+
+    public float minMax(Board board, Move move, int depth, Mark maxPlayer) {
+        board.play(move, maxPlayer);
+
+        if (depth == 0) {
+            return board.evaluate(maxPlayer);
+        }
+
+        float maxEval;
+        float minEval;
+
+        Mark opPlayer;
+
+        switch (maxPlayer) {
+            case X:
+                opPlayer = Mark.O;
+                break;
+            case O:
+                opPlayer = Mark.X;
+                break;
+            default:
+                opPlayer = Mark.EMPTY;
+                break;
+        }
+
+        if (player.equals(maxPlayer)) {
+            maxEval = Float.NEGATIVE_INFINITY;
+
+            for (Move childMove: board.listMoves(maxPlayer)) {
+                float eval = minMax(board.clone(), childMove, depth - 1, opPlayer);
+                maxEval = Float.max(maxEval, eval);
+            }
+
+            return maxEval;
+        } else {
+            minEval = Float.POSITIVE_INFINITY;
+
+            for (Move childMove: board.listMoves(maxPlayer)) {
+                float eval = minMax(board.clone(), childMove, depth - 1, opPlayer);
+                minEval = Float.min(minEval, eval);
+            }
+
+            return minEval;
+        }
     }
 
 }
